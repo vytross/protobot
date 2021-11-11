7468e5f40536b7fb8525ffc907769aad3e6c4b91
vytross <71793587+vytross@users.noreply.github.com>
2021-11-10 21:27:10 -0500

    Add files via upload

--- commands/changelog.js
+++ commands/changelog.js
@@ -9,12 +9,11 @@ module.exports = {
 	    const embed = new MessageEmbed()
 	        .setColor('#E133FF')
 	        .setAuthor('GitHub', 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png', 'https://github.com/vytross/prototech-games')
-	        .setFooter('ProtoTech Games', 'https://cdn.discordapp.com/attachments/793289338112835588/896543216017502268/protoshrek.png')
-	    	.setURL('https://github.com/vytross/prototech-games/blob/main/README.md')
+	        .setFooter('ProtoBot', 'https://cdn.discordapp.com/attachments/793289338112835588/896543216017502268/protoshrek.png')
+	        .setURL('https://github.com/vytross/protobot/blob/main/README.md')
 	        .setTitle('Change Log')
 	        .addFields(
-	            {name: 'Pre-Release v0.0.1', value: '• Fixed tic-tac-toe algorithm\n• Added restart and delete reaction messages to finished games\n• Added /changelog command'},
-	            {name: 'Pre-Release v0.0.0', value: '• Introduced the bot to Discord!\n• Added new game tic-tac-toe\n• Added /ping command\n• Added /info command\n• Added /channel command\n• Added /game command'}
+	            {name: 'Pre-Release v0.1.0', value: '• Rebranded to ProtoBot\n• Incorporated ProtoTech Applications\n• Improved admin-side application control usability\n• Bug fixes for tic-tac-toe crashes\n• Added /application command'}
 	        )
 	        .setTimestamp();
 		await interaction.reply({embeds: [embed]});
