c1e1c107de61b6b9c429f19582f9d16958fded93
vytross <71793587+vytross@users.noreply.github.com>
2021-11-10 21:43:51 -0500

    Add files via upload

--- commands/info.js
+++ commands/info.js
@@ -8,14 +8,14 @@ module.exports = {
 	async execute(interaction) {
 	    const embed = new MessageEmbed()
 	        .setColor('#E133FF')
-	        .setAuthor('GitHub', 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png', 'https://github.com/vytross/prototech-games')
-	        .setFooter('ProtoTech Games', 'https://cdn.discordapp.com/attachments/793289338112835588/896543216017502268/protoshrek.png')
+	        .setAuthor('GitHub', 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png', 'https://github.com/vytross/protobot')
+	        .setFooter('ProtoBot', 'https://cdn.discordapp.com/attachments/793289338112835588/896543216017502268/protoshrek.png')
 	        .setTitle('Information Board')
-	        .setDescription('Built and maintained by vytross, ProtoTech Games adds a few in-app Discord games for users on the ProtoTech Discord server to play amongst themselves and against the bot. This bot is constantly being worked on and updated, so look out for new and exciting things coming soon!')
+	        .setDescription('Built and maintained by vytross, ProtoBot adds a better application system along with a few in-app Discord games for users on the ProtoTech Discord server to play amongst themselves and against the bot. This bot is constantly being worked on and updated, so look out for new and exciting things coming soon!')
 	        .addFields(
-	            {name: 'Command List', value: '/info\n/ping\n/game', inline: true},
+	            {name: 'Public Command List', value: '/info\n/ping\n/game\n/changelog', inline: true},
 	            {name: 'Current Games', value: 'Tic-Tac-Toe', inline: true},
-	            {name: 'In Conjunction With', value: 'ProtoTech Applications', inline: true}
+	            {name: 'Comprised Of', value: 'ProtoTech Applications\nProtoTech Games', inline: true}
 	        )
 	        .setTimestamp();
 		await interaction.reply({embeds: [embed]});
